e9ed5c5b85d2731d18eff40c06721af3ea991df8
SP1K3Y-EY2 <2200579@uad.ac.uk>
2025-09-25 15:52:16 +0100

    Rotation and Charge

--- Assets/Scripts/KickBallScript.cs
+++ Assets/Scripts/KickBallScript.cs
@@ -1,27 +1,43 @@
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class KickBallScript : MonoBehaviour
 {
+    InputAction launchAction;
+
     float hCharge = 0; //hCharge will be in the form of rotation and bounce between -100 and 100.
     int hChargeDirection = 1;
-    float halfMaxAngle = 45;
+    float hChargeRate = 0.2f;
+    float halfMaxAngle = 45f;
 
     float power = 0;
+    int powerChargeDirection = 1;
+    float powerChargeRate = 0.2f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        launchAction = InputSystem.actions.FindAction("Attack");
+        launchAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        hCharge += 1 * hChargeDirection;
+        if (launchAction.IsPressed())
+        {
+            power += powerChargeRate * powerChargeDirection;
 
-        transform.rotation = Quaternion.Euler(0, 0, hCharge * halfMaxAngle);
+            if (power <= 0 || power >= 100) powerChargeDirection *= -1;
+        }
+        else
+        {
+            hCharge += hChargeRate * hChargeDirection;
 
-        if (hCharge >= 100 || hCharge <= 0) hChargeDirection *= -1;
+            transform.rotation = Quaternion.Euler(0, 0, (hCharge / 100f) * halfMaxAngle);
+
+            if (hCharge <= -100 || hCharge >= 100) hChargeDirection *= -1;
+        }
     }
 }
